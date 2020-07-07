b5ca640078d545c5e21484409161e2824bab5bfb
Erhan Ince <erhanince@Erhans-MacBook-Pro.local>
2020-07-07 14:52:24 +0200

    Data-Types: Lev1.151

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,3 +42,29 @@ console.log(typeof sym);
 let addition = 3 + 2 == 5;
 console.log(typeof addition);
 let addi = 3 + '3';
+
+// -----------------------Lev1_14------------------------
+console.log(Boolean(100));
+console.log(Boolean(3.14));
+console.log(Boolean(-15));
+console.log(Boolean('hello'));
+console.log(Boolean('false'));
+console.log(Boolean(1 + 7 + 3.14));
+console.log(Boolean(0));
+console.log(Boolean(-0));
+console.log(Boolean(NaN));
+console.log(Boolean(null));
+console.log(Boolean(false));
+console.log(Boolean('Nan'));
+
+// ---------------------------Lev1_15 ----------------------
+let num = 5;
+let anotherNum = 8;
+console.log(num == anotherNum);
+console.log(num > anotherNum);
+console.log(num < anotherNum);
+console.log(num >= anotherNum);
+console.log(num <= anotherNum);
+console.log(num == '5');
+console.log(num != 8);
+console.log(num != '5');
