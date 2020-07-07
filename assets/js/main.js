a9727e8c693d01a717e845c8f9758988142b06ac
Erhan Ince <erhanince@Erhans-MacBook-Pro.local>
2020-07-07 14:39:36 +0200

    Data-Types: Lev1

---
+++ assets/js/main.js
@@ -0,0 +1,44 @@
+var i = 1;
+console.log(typeof i);
+var lastName = 'Johnson';
+console.log(typeof lastName);
+var x = {
+     firstName: 'John',
+     lastName: 'Doe',
+};
+console.log(typeof x);
+let j = '2';
+console.log(typeof j);
+let status = true;
+console.log(typeof status);
+let hello = 'hello';
+console.log(typeof hello);
+
+let helloWorld = 'hello';
+console.log(typeof helloWorld);
+let name = 'John';
+console.log(typeof name);
+let pi = 3.14;
+console.log(typeof pi);
+let numb = NaN;
+console.log(typeof NaN);
+let val = false;
+console.log(typeof val);
+let array = [1, 2, 3, 4];
+console.log(typeof array);
+let data = { name: 'John', age: 34 };
+console.log(typeof data);
+let date = new Date();
+console.log(typeof date);
+let func = function () {};
+console.log(typeof func);
+let car = ' myCar';
+console.log(typeof car);
+
+let zero = null;
+console.log(typeof zero);
+let sym = '';
+console.log(typeof sym);
+let addition = 3 + 2 == 5;
+console.log(typeof addition);
+let addi = 3 + '3';
